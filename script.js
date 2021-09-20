fe44ef249e8a6067a30d15380568e350023087d9
petitejess <jessica.gozali@outlook.com.au>
2021-09-20 16:45:26 +1000

    Test happy button to console log all quotes

--- script.js
+++ script.js
@@ -1,41 +1,36 @@
-function buttons() {
-    const happyMood = document.getElementById('feelings');
-    const sadMood = document.getElementById('feelings_sad');
-    const excitedMood = document.getElementById('feelings_excited');
-    const tiredMood = document.getElementById('feelings_tired');
-    const scaredMood = document.getElementById('feelings_care');
-    const anxiousMood = document.getElementById('feelings_anxious');
-    const confusedMood = document.getElementById('feelings_confused');
-    const boredMood = document.getElementById('feelings_bored');
-    console.log(happyMood);
+const happyMood = document.getElementById('feelings');
+const sadMood = document.getElementById('feelings_sad');
+const excitedMood = document.getElementById('feelings_excited');
+const tiredMood = document.getElementById('feelings_tired');
+const scaredMood = document.getElementById('feelings_care');
+const anxiousMood = document.getElementById('feelings_anxious');
+const confusedMood = document.getElementById('feelings_confused');
+const boredMood = document.getElementById('feelings_bored');
+
+function showQuote(quotes) {
+  console.log(quotes);
 }
-
-buttons()
-
-happyMood.addEventListener("click", function(event) {
-    let entry = getRandomInt(10)
-    event.preventDefault()
-    console.log(entry)
-})
-
-
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
-function getQuote() {
+async function getQuote() {
     var requestOptions = {
       method: 'GET',
       redirect: 'follow'
     };
   
-    fetch("https://type.fit/api/quotes", requestOptions)
-      .then(response => response.json())
-      .then(result => (result))
-      .catch(error => console.log('error', error))
+    try {
+      let response = await fetch("https://type.fit/api/quotes", requestOptions);
+      let quotes = await response.json();
+      showQuote(quotes);
+    }
+    catch(err) {
+      console.log(err);
+    }
+}
 
-      
-  }
-  
+happyMood.addEventListener("click", () => {
   getQuote();
+});
